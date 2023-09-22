327b177687d0bb431c62cb150645398ca527441f
Florin12er <Sebastianflorin603@gmail.com>
2023-09-22 18:48:11 +0200

    make the design of the calculator

---
+++ calculator.js
@@ -0,0 +1,49 @@
+// Constants for display and buttons
+const display = document.querySelector("div.display");
+
+const operators = {
+  Plus: document.querySelectorAll("div.Plus"),
+  Minus: document.querySelectorAll("div.Minus"),
+  Multiply: document.querySelectorAll("div.Multiply"),
+  Divide: document.querySelectorAll("div.Division")
+};
+
+const numbers = {
+  One: document.querySelectorAll("div.One"),
+  Two: document.querySelectorAll("div.Two"),
+  Three: document.querySelectorAll("div.Three"),
+  Four: document.querySelectorAll("div.Four"),
+  Five: document.querySelectorAll("div.Five"),
+  Six: document.querySelectorAll("div.Six"),
+  Seven: document.querySelectorAll("div.Seven"),
+  Eight: document.querySelectorAll("div.Eight"),
+  Nine: document.querySelectorAll("div.Nine"),
+  Zero: document.querySelectorAll("div.Zero")
+};
+
+// Buttons for calculations
+const plusButton = document.querySelector("#PlusCross");
+const minusButton = document.querySelector("#MinusCross");
+const multiplyButton = document.querySelector("#MultiplyCross");
+const divideButton = document.querySelector("#DivisionCross");
+const equalButton = document.querySelector("#EqualButton");
+const clearButton = document.querySelector("#C");
+const decimalButton = document.querySelector("#decimal");
+const plusMinusButton = document.querySelector("#plusMinusButton");
+
+// Equation functions
+function add() {
+ 
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
